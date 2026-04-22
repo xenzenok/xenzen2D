fa7df185de07ec93b0febb1910d6bb9ff68c6a10
XalebXEn99 <150833019+XalebXEn99@users.noreply.github.com>
2026-04-22 17:56:13 +0200

    folder setup + empty room in heaven

--- project/scripts/test/npc.gd
+++ project/scripts/test/npc.gd
@@ -24,4 +24,4 @@ func pick_random_direction() -> Vector2:
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		print("Player touched NPC!")
-		get_tree().change_scene_to_file("res://scenes/Battle.tscn")
+		get_tree().change_scene_to_file("res://scenes/test/Battle.tscn")
--- project/scripts/test/timer.gd
+++ project/scripts/test/timer.gd
@@ -4,4 +4,4 @@ func _ready():
 	$Timer.start()
 
 func _on_timer_timeout():
-	get_tree().change_scene_to_file("res://scenes/Win.tscn")
+	get_tree().change_scene_to_file("res://scenes/test/Win.tscn")
